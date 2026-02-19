4a091e74e85eb06aa483589319ac27f5953b6d4d
DESKTOP-B0MRVUR\eghis <syey86@eghishc.co.kr>
2026-02-19 16:28:54 +0900

    1. Request에 emplNo 항목 추가 - [병원정보관리 > 의료진관리]주간 스케쥴 수정 API(doctor/weeks-schedule) - [병원정보관리 > 의료진관리]지정 스케쥴 수정 API(doctor/days-schedule) - [병원정보관리 > 의료진관리]비대면 스케쥴 수정 API(doctor/untact-weeks-schedule) 2. [병원정보관리 > 의료진관리]지정 스케쥴 수정 API(doctor/days-schedule) 스케쥴 제거 로직 추가

--- src/API/Constracts/Admin/HospitalManagement/PatchDoctorWeeksScheduleRequest.cs
+++ src/API/Constracts/Admin/HospitalManagement/PatchDoctorWeeksScheduleRequest.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Hello100Admin.API.Constracts.Admin.HospitalManagement
+﻿namespace Hello100Admin.API.Constracts.Admin.HospitalManagement
 {
     public class PatchDoctorWeeksScheduleInfo
     {
